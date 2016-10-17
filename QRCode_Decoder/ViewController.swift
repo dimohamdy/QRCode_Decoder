b436b7bc10e895e4a058a0aa17ecef811e3b1b47
dimohamdy <dimo.hamdy@gmail.com>
2016-10-17 15:01:17 +0200

    Initial Commit

---
+++ QRCode_Decoder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRCode_Decoder
+//
+//  Created by BinaryBoy on 10/17/16.
+//  Copyright © 2016 BinaryBoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
